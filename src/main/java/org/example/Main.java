f20953262d64606d13c88f062b4814355803b1c8
Zeshan Shahid <z.shahid@hotmail.de>
2022-10-06 12:03:55 +0200

    testing branches

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Zeshan!");
         System.out.println("creating branches and push on different head´s");
+        System.out.println("Hello World");
     }
 }
