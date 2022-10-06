81344ea3874381a2de48dcc961e566ccc14b3e8d
davidskottke <davidskottke@googlemail.com>
2022-10-06 16:34:54 +0200

    add hallo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,5 +29,6 @@ public class Main {
         System.out.println("Zeshan Shahid");
         System.out.println(zeshansArray);
         System.out.println(listOfNames);
+        System.out.println("hallo !!!!!");
     }
 }
